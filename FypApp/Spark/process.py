3306a20b4a030eec6bcf42c7e9c9bd698910d55c
chanzs415 <127963294+chanzs415@users.noreply.github.com>
2023-03-23 16:07:34 +0800

    Update process.py

--- FypApp/Spark/process.py
+++ FypApp/Spark/process.py
@@ -1,13 +1,65 @@
+from pyspark.sql.functions import *
+from pyspark.ml.feature import Imputer, StringIndexer, IndexToString, OneHotEncoder
 from pyspark.sql import SparkSession
 
 # create SparkSession
 spark = SparkSession.builder.appName("MyApp").getOrCreate()
+# Read in csv file
+df = spark.read.format("csv").option("header", "true").load("hdfs://namenode:9000/data/combined_csv.csv")
 
-df = spark.read.csv("hdfs://namenode:9000/data/data/*.csv", header = True)
-lessNames = ["tempmax", "tempmin"]
+# Check for duplicates
+duplicates = df.groupBy(*df.columns).count().filter(col("count") > 1)
 
-dfNew = df[lessNames]
-dfNew.show(10)
-newDF = dfNew.write.csv('/usr/local/output3', mode='overwrite', header=True)
+if duplicates.count() > 0:
+    print(f"There are {duplicates.count()} duplicates in the DataFrame.")
+else:
+    print("There are no duplicates in the DataFrame.")
 
+# Check for missing values
+df1 = df.select([count(when(col(c).isNull() | (col(c) == ""), c)).alias(c) for c in df.columns])
+df1.show()
+
+# Convert the "preciptype" column from String to index
+indexer = StringIndexer(inputCol="preciptype", outputCol="preciptype_idx", handleInvalid="skip")
+
+from pyspark.ml.feature import Imputer, StringIndexer
+
+# Convert the "preciptype" column from String to index
+indexer = StringIndexer(inputCol="preciptype", outputCol="preciptype_idx", handleInvalid="skip")
+df2 = indexer.fit(df).transform(df)
+
+# Cast columns to Double
+df3 = df2.withColumn("uvindex_double", col("uvindex").cast("double")) \
+         .withColumn("severerisk_double", col("severerisk").cast("double")) \
+         .withColumn("windgust_double", col("windgust").cast("double")) \
+         .withColumn("solarradiation_double", col("solarradiation").cast("double")) \
+         .withColumn("solarenergy_double", col("solarenergy").cast("double"))
+
+# Impute missing values for categorical variables
+imputer_cate = Imputer(inputCols=["uvindex_double", "severerisk_double", "preciptype_idx"],
+                       outputCols=["uvindex_imp", "severerisk_imp", "preciptype_imp"],
+                       strategy="median")
+df_cate = imputer_cate.fit(df3).transform(df3)
+
+# Impute missing values for continuous variables
+imputer_cont = Imputer(inputCols=["windgust_double", "solarradiation_double", "solarenergy_double"],
+                       outputCols=["windgust_imp", "solarradiation_imp", "solarenergy_imp"],
+                       strategy="mean")
+df_cont = imputer_cont.fit(df_cate).transform(df_cate)
+
+# Drop unnecessary columns and rename columns
+df_new = df_cont.drop("preciptype", "uvindex", "severerisk", "windgust", "solarradiation", "solarenergy", 
+                       "uvindex_double", "severerisk_double", "preciptype_idx", "windgust_double", 
+                       "solarradiation_double", "solarenergy_double")
+df_clean = df_new.withColumnRenamed("uvindex_imp", "uvindex") \
+                 .withColumnRenamed("severerisk_imp", "severerisk") \
+                 .withColumnRenamed("preciptype_imp", "preciptype") \
+                 .withColumnRenamed("windgust_imp", "windgust") \
+                 .withColumnRenamed("solarradiation_imp", "solarradiation") \
+                 .withColumnRenamed("solarenergy_imp", "solarenergy")
+
+# Write the cleaned dataframe to csv
+df_clean.write.mode('overwrite').csv('/usr/local/output', header=True)
+
+spark.stop()
 
